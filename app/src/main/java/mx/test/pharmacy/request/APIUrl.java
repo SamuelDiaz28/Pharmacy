8595475e5488ae00031ae317d2ee1d52d988518e
abibarte <abibarte@gmail.com>
2021-06-17 20:37:53 -0500

    Se coloca servicio correcto para mostrar los medicamentos por nombre.

--- app/src/main/java/mx/test/pharmacy/request/APIUrl.java
+++ app/src/main/java/mx/test/pharmacy/request/APIUrl.java
@@ -4,7 +4,7 @@ public class APIUrl {
     public static final String BASE = "http://172.241.131.237:9090/farmacias/";
 
 
-    public static final String GET_MEDICINES = "medicamentos/findByName/{SearchWord}";
+    public static final String GET_MEDICINES = "medicamentos/findByNameDistinct/{SearchWord}";
 
     public static final String GET_MEDICAMENTOS = "farmacias/medicamentos";
 
